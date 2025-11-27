b6e222c9159ad45f1391949d1486d7a466ddeb1d
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-11-27 16:40:38 -0600

    Update defensas.h

--- defensas.h
+++ defensas.h
@@ -1,61 +1,45 @@
+// Herencia para los Defensas del Cruz Azul
+#ifndef CAZDEFENSAS_H_
+#define CAZDEFENSAS_H_
+
 #include <iostream>
 #include <string>
+#include "Jugador.h"
+
 using namespace std;
 
 // Defensas del Cruz Azul
-class CAZdefensas {
+class CAZdefensas: public Jugador{
 
 private:
-    string nombrejug;
-    int dorsaljug;
     int balonesbloqueados;
 
 public:
     // Constructor 
-    CAZdefensas (string nm = "", int dj = 0, int bb = 0)
-    : nombrejug(nm), dorsaljug(dj), balonesbloqueados(bb){}
+    CAZdefensas(): Jugador("", 0), balonesbloqueados(0){}
+    CAZdefensas(string nm, int dj, int bb)
+    :Jugador(nm, dj), balonesbloqueados (bb){}
 
     // Set
-    void setNombres(string nm){
-        nombrejug = nm;
-    }
-
-    void setDorsal(int dj){
-        if (dj > 0 && dj <= 99)
-            dorsaljug = dj;
-        else
-            cout << "El Dorsal Tiene que ser un Numero del 1 al 99" << endl;
-    }
-
     void setBalonesBloqueados(int bb){
         if (bb >= 0)
             balonesbloqueados =  bb;
         else
-            cout << "Los balones bloqueados no pueden ser negativas" << endl;
+            cout << "Los balones bloqueados no pueden ser negativos" << endl;
     }
 
     // Get
 
-    string getNombres(){
-        return nombrejug;
-    }
-
-    int getDorsal(){
-        return dorsaljug;
-    }
-
-    int getBalonesBloquedos(){
+    int getBalonesBloqueados(){
         return balonesbloqueados;
     }
 
     // Mostrar Info
 
     void mostrarinfo(){
-        cout << "\n ---Defensa Del Cruz Azul---" << endl;
-        cout << "Nombre del Defensa : " << nombrejug << endl;
-        cout << "Dorsal del Defensa : " << dorsaljug << endl;
-        cout << "Los Balones Bloqueados de " << nombrejug 
-        << " Son de : " << balonesbloqueados << endl;
+        cout << "\n Defensa Del Cruz Azul" << endl;
+        Jugador :: mostrarinfo();
+        cout << "Los Balones Bloqueados son : " << balonesbloqueados << endl;
     }
-
 };
+#endif // CAZDEFENSAS_H_
