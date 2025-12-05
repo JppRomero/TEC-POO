a3430c0f0bb881acf3be9d231af1f708b8fc859e
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:08:12 -0600

    Refactor CAZporteros.h with improved comments
    Updated comments and formatting in CAZporteros.h

--- CAZporteros.h
+++ CAZporteros.h
@@ -1,31 +1,40 @@
-// Herencia para los Porteros del Cruz Azul
+// CAZporteros.h el cual contiene la definicion
+// de la clase CAZporteros 
+// que hereda de la clase Jugador,
+// agregando atributos y metodos 
+// especificos para los porteros.
+
 #ifndef CAZPORTEROS_H_
 #define CAZPORTEROS_H_
-
+// Inclusiones necesarias.
 #include <iostream>
 #include <string>
 #include "Jugador.h"
 
 using namespace std;
 
-// Porteros del Cruz Azul
-class CAZporteros: public Jugador{
+// Definicion de la clase CAZporteros, 
+// el cual hereda de la clase Jugador.
 
+class CAZporteros: public Jugador{
+// Atributos privados especificos de los porteros.
 private:
     int balonesparados;
-
+// Ponemos los metodos publicos de la clase CAZporteros.
 public:
-    // Constructor 
+    // Constructor el cual inicializa los atributos.
     CAZporteros(string nm = "", int dj = 0, int bp = 0);
 
-    // Set
+    // Creamos el Setter necesario.
     void setBalonesParados(int bp);
 
-    // Get
+    // Creamos el Getter necesario.
     int getBalonesParados();
 
-    // Mostrar Info
+    // Creamos el metodo para mostrar 
+    // la informacion del portero.
 
     void mostrarinfo();
 };
+// Fin de la definicion de la clase CAZporteros.
 #endif // CAZPORTEROS_H_
