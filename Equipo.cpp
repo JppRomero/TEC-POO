b64971a7cdf35ae4aa1111891c90e9c15829b365
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:07:45 -0600

    Refactor methods and comments in Equipo class
    Refactor implementation of Equipo class methods.

--- Equipo.cpp
+++ Equipo.cpp
@@ -1,36 +1,47 @@
+//Equipo.cpp el cual contiene la implementacion
+// de los metodos de la clase Equipo.
+
 #include "Equipo.h"
 #include <iostream>
 #include <string>
 using namespace std;
 // Implementacion de los metodos de la clase Equipo
-// Constructor
-Equipo::Equipo() : cantPorteros(0), cantDefensas(0), cantMedios(0), cantDelanteros(0) {}
-// Metodos para agregar a los jugadores
+// Llamado del constructor de la clase Equipo.
+Equipo::Equipo() : cantPorteros(0), cantDefensas(0), 
+cantMedios(0), cantDelanteros(0) {}
+
+// Llamado de los metodos para agregar jugadores al equipo.
+
+// Agregamos un portero al equipo
 void Equipo::agregarPortero(string nm, int dj, int bp){
     if (cantPorteros < 3){
         porteros[cantPorteros] = CAZporteros(nm, dj, bp);
         cantPorteros++;
     }
 }
+// Agregamos un defensa al equipo
 void Equipo::agregarDefensa(string nm, int dj, int bb){
     if (cantDefensas < 7){
         defensas[cantDefensas] = CAZdefensas(nm, dj, bb);
         cantDefensas++;
     }
 }
+// Agregamos un mediocampista al equipo
 void Equipo::agregarMedio(string nm, int dj, int p){
     if (cantMedios < 13){
         medios[cantMedios] = CAZmedios(nm, dj, p);
         cantMedios++;
     }
 }
+// Agregamos un delantero al equipo
 void Equipo::agregarDelantero(string nm, int dj, int g){
     if (cantDelanteros < 3){
         delanteros[cantDelanteros] = CAZdelanteros(nm, dj, g);
         cantDelanteros++;
     }
 }
-// Mostramos a todo el equipo
+
+// Llamado del metodo para mostrar la informacion del equipo.
 void Equipo::mostrarEquipo(){
     cout << "\n--- Equipo de Cruz Azul ---\n" << endl;
     cout << "\n--- Porteros ---" << endl;
@@ -50,3 +61,4 @@ void Equipo::mostrarEquipo(){
         delanteros[i].mostrarinfo();
     }
 }
+// Fin de la implementacion de los metodos de la clase Equipo.
