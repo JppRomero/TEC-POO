1031c0bdbb6a6ca7c4b993df8836e18025412b6d
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:08:58 -0600

    Refactor CAZmedios.h comments and structure
    Updated comments and formatting for clarity.

--- CAZmedios.h
+++ CAZmedios.h
@@ -1,32 +1,40 @@
-// Herencia para los Mediocampistas del Cruz Azul
+// CAZmedios.h el cual contiene la definicion
+// de la clase CAZmedios
+// que hereda de la clase Jugador,
+// agregando atributos y metodos
+// especificos para los mediocampistas.
 
 #ifndef CAZMEDIOS_H_
 #define CAZMEDIOS_H_
-
+// Inclusiones necesarias.
 #include <iostream>
 #include <string>
 #include "Jugador.h"
 
 using namespace std;
 
-// MedioCampistas del Cruz Azul
-class CAZmedios: public Jugador{
+// Definicion de la clase CAZmedios, 
+// el cual hereda de la clase Jugador.
 
+class CAZmedios: public Jugador{
+// Atributos privados especificos de los mediocampistas.
 private:
     int pases;
-
+// Ponemos los metodos publicos de la clase CAZmedios.
 public:
-    // Constructor 
+    // Constructor el cual inicializa los atributos.
     CAZmedios (string nm = "", int dj = 0, int p = 0);
 
-    // Set
+    // Creamos el Setter necesario.
     void setPases(int p);
 
-    // Get
+    // Creamos el Getter necesario.
     int getPases();
 
-    // Mostrar Info
+    // Creamos el metodo para mostrar
+    // la informacion del mediocampista.
 
     void mostrarinfo();
 };
+// Fin de la definicion de la clase CAZmedios.
 #endif // CAZMEDIOS_H_
