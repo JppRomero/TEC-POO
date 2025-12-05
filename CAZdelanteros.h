265aa4c6c7a29e2d27734028f67df944c4fc617a
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:09:39 -0600

    Update CAZdelanteros.h

--- CAZdelanteros.h
+++ CAZdelanteros.h
@@ -1,32 +1,41 @@
-// Herencia para los Delanteros del Cruz Azul
+// CAZdelanteros.h el cual contiene la definicion
+// de la clase CAZdelanteros
+// que hereda de la clase Jugador,
+// agregando atributos y metodos
+// especificos para los delanteros.
+
 #ifndef CAZDELANTEROS_H_
 #define CAZDELANTEROS_H_
-
+// Inclusiones necesarias.
 #include <iostream>
 #include <string>
 #include "Jugador.h"
 using namespace std;
 
-// Delanteros del Cruz Azul
-class CAZdelanteros: public Jugador{
+// Definicion de la clase CAZdelanteros, 
+// el cual hereda de la clase Jugador.
 
+class CAZdelanteros: public Jugador{
+// Atributos privados especificos de los delanteros.
 private:
     int goles;
-
+// Ponemos los metodos publicos de la clase CAZdelanteros.
 public:
-    // Constructor 
+    // Constructor el cual inicializa los atributos.
     CAZdelanteros (string nm = "", int dj = 0, int g = 0);
 
-    // Set
+    // Creamos el Setter necesario.
 
     void setGoles(int g);
 
-    // Get
+    // Creamos el Getter necesario.
 
     int getGoles();
 
-    // Mostrar Info
+    // Creamos el metodo para mostrar
+    // la informacion del delantero.
 
     void mostrarinfo();
 };
+// Fin de la definicion de la clase CAZdelanteros.
 #endif // CAZDELANTEROS_H_
