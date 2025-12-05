c68b81720b67f2215104ce4ced80b625e5ecbb69
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:08:32 -0600

    Enhance comments in CAZporteros.cpp
    Added comments to clarify the implementation of methods.

--- CAZporteros.cpp
+++ CAZporteros.cpp
@@ -1,24 +1,28 @@
+// CAZporteros.cpp el cual contiene la implementacion
+// de los metodos de la clase CAZporteros.
+
 #include "CAZporteros.h"
 #include <iostream>
 #include <string>
 using namespace std;
 // Implementacion de los metodos de la clase CAZporteros
-// Constructor
+// Llamado del constructor de la clase CAZporteros.
 CAZporteros::CAZporteros(string nm, int dj, int bp) 
     :Jugador (nm, dj), balonesparados (bp) {}
-// Set
+    
+// Llamado del Setter de la clase CAZporteros.
 void CAZporteros::setBalonesParados(int bp){
     balonesparados = bp;
 
 }
-// Get
+// Llamado del Getter de la clase CAZporteros.
 int CAZporteros::getBalonesParados(){
     return balonesparados;
 }
-// Mostrar Info
+// Llamado del metodo mostrarinfo de la clase CAZporteros.
 void CAZporteros::mostrarinfo(){
     cout << "\n Portero Del Cruz Azul" << endl;
     Jugador :: mostrarinfo();
     cout << "Las Atajadas del portero es de: "<< balonesparados << endl;
 }
-
+// Fin de la implementacion de los metodos de la clase CAZporteros.
