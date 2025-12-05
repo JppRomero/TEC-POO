56c27c174873fb3501b3487cb9a876ab69620d08
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:05:44 -0600

    Refactor comments and structure in Jugador.h
    Updated comments and formatting in Jugador.h for clarity.

--- Jugador.h
+++ Jugador.h
@@ -1,36 +1,43 @@
-// Clase base Jugador.h la cual contiene los atributos y metodos comunes
-// y se usa como clase base para la herencia en los diferentes tipos de jugadores.
+// Jugador.h el cual es la clase base 
+// para los diferentes tipos de jugadores
+// el cual contiene atributos comunes como nombre 
+// y dorsal, asi como metodos para
+// establecer y obtener estos atributos, y 
+// un metodo para mostrar la informacion del jugador.
 
 #ifndef JUGADOR_H_
 #define JUGADOR_H_
-
+// Inclusiones necesarias
 #include <iostream>
 #include <string>
 using namespace std;
-// Clase Base Jugador 
+// Definicion de la clase Jugador
 
 class Jugador{
-
+// Atributos los cuales son protegidos para 
+// seguir el principio de encapsulamiento y herencia.
 protected:
     string nombrejug;
     int dorsaljug;
+// Metodos publicos.
+
 public:
-    // constructor
+    // Constructor el cual inicializa los atributos.
     Jugador (string nm = "", int dj = 0);
     
-    // Set
+    // Creamos los Setters necesarios.
     void setNombres(string nm);
 
     void setDorsal(int dj);
 
-    // Get
-
+    // Creamos los Getters necesarios.
     string getNombres();
     int getDorsal();
     
-    // Mostrar Info
+    // Creamos el metodo para mostrar 
+    // la informacion del jugador.
 
     void mostrarinfo();
 };
-
+// Fin de la definicion de la clase Jugador
 #endif // JUGADOR_H_
