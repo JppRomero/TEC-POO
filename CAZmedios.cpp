92f75c4967694e69eb5e6d6fb079eec90680b0f2
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:09:17 -0600

    Refactor CAZmedios.cpp with comments and formatting

--- CAZmedios.cpp
+++ CAZmedios.cpp
@@ -1,22 +1,27 @@
+// CAZmedios.cpp el cual contiene la implementacion
+// de los metodos de la clase CAZmedios.
+
 #include "CAZmedios.h"
 #include <iostream>
 #include <string>
 using namespace std;
 // Implementacion de los metodos de la clase CAZmedios
-// Constructor
+// Llamado del constructor de la clase CAZmedios.
 CAZmedios::CAZmedios (string nm, int dj, int p)
     :Jugador (nm, dj), pases (p) {}
-// Set
+
+// Llamado del Setter de la clase CAZmedios.
 void CAZmedios::setPases(int p){
     pases = p;
 }
-// Get
+// Llamado del Getter de la clase CAZmedios.
 int CAZmedios::getPases(){
     return pases;
 }
-// Mostrar Info
+// Llamado del metodo mostrarinfo de la clase CAZmedios.
 void CAZmedios::mostrarinfo(){
     cout << "\n Medio Del Cruz Azul" << endl;
     Jugador :: mostrarinfo();
     cout << "Los Pases del Jugador son de : " << pases << endl;
 }
+// Fin de la implementacion de los metodos de la clase CAZmedios.
