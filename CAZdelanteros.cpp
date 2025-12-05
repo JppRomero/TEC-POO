0531cfb6beeb3f6a510d8145f1651b3704cb9406
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:10:00 -0600

    Update CAZdelanteros.cpp

--- CAZdelanteros.cpp
+++ CAZdelanteros.cpp
@@ -1,22 +1,27 @@
+// CAZdelanteros.cpp el cual contiene la implementacion
+// de los metodos de la clase CAZdelanteros.
+
 #include "CAZdelanteros.h"
 #include <iostream>
 #include <string>
 using namespace std;
 // Implementacion de los metodos de la clase CAZdelanteros
-// Constructor
+// Llamado del constructor de la clase CAZdelanteros.
 CAZdelanteros::CAZdelanteros (string nm, int dj, int g)
     :Jugador (nm,dj), goles(g) {}
-// Set
+
+// Llamado del Setter de la clase CAZdelanteros.
 void CAZdelanteros::setGoles(int g){
     goles = g;
 }
-// Get
+// Llamado del Getter de la clase CAZdelanteros.
 int CAZdelanteros::getGoles(){
     return goles;
 }
-// Mostrar Info
+// Llamado del metodo mostrarinfo de la clase CAZdelanteros.
 void CAZdelanteros::mostrarinfo(){
     cout << "\n Delantero Del Cruz Azul" << endl;
     Jugador :: mostrarinfo();
     cout << "Los Goles del Jugador son de : " << goles << endl;
 }
+// Fin de la implementacion de los metodos de la clase CAZdelanteros.
