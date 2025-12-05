b60e53c182c7f2963d6b1d7aaa4e3ab3ff90ce50
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:10:39 -0600

    Update CAZdefensas.cpp

--- CAZdefensas.cpp
+++ CAZdefensas.cpp
@@ -1,22 +1,27 @@
+// CAZdefensas.cpp el cual contiene la implementacion 
+// de los metodos de la clase CAZdefensas.
+
 #include "CAZdefensas.h"
 #include <iostream>
 #include <string>
 using namespace std;
 // Implementacion de los metodos de la clase CAZdefensas
-// Constructor
+// Llamado del constructor de la clase CAZdefensas.
 CAZdefensas::CAZdefensas(string nm, int dj, int bb) 
     :Jugador (nm, dj), balonesbloqueados (bb) {}
-// Set
+
+// Llamado del Setter de la clase CAZdefensas.
 void CAZdefensas::setBalonesBloqueados(int bb){
     balonesbloqueados = bb;
 }
-// Get
+// Llamado del Getter de la clase CAZdefensas.
 int CAZdefensas::getBalonesBloqueados(){
     return balonesbloqueados;
 }
-// Mostrar Info
+// Llamado del metodo mostrarinfo de la clase CAZdefensas.
 void CAZdefensas::mostrarinfo(){
     cout << "\n Defensa Del Cruz Azul" << endl;
     Jugador :: mostrarinfo();
     cout << "Los Balones Bloqueados son : " << balonesbloqueados << endl;
 }
+// Fin de la implementacion de los metodos de la clase CAZdefensas.
