d3413aa0155c98c66bb00107a6be17ac81701063
Juan Pablo <132721368+JppRomero@users.noreply.github.com>
2025-12-04 18:06:21 -0600

    Refactor Jugador.cpp with comments and formatting

--- Jugador.cpp
+++ Jugador.cpp
@@ -1,26 +1,33 @@
+// Jugador.cpp el cual contiene la implementacion
+// de los metodos de la clase base Jugador.
+
 #include "Jugador.h"
 #include <iostream>
 #include <string>
 using namespace std;
-// Implementacion de los metodos de la clase base Jugador
-// Constructor
+// Implementacion de los metodos de la clase base Jugador.
+// Llamado de constructor de la clase Jugador.
 Jugador::Jugador (string nm, int dj) : nombrejug(nm), dorsaljug(dj) {}
-// Set
+
+// Llamado de los Setters de la clase Jugador.
 void Jugador::setNombres(string nm){
     nombrejug = nm;
 }
 void Jugador::setDorsal(int dj){
     dorsaljug = dj;
 }
-// Get
+
+// Llamado de los Getters de la clase Jugador.
 string Jugador::getNombres(){
     return nombrejug;
 }
 int Jugador::getDorsal(){
     return dorsaljug;
 }
-// Mostrar Info
+
+// Llamado de el metodo mostrarinfo de la clase Jugador.
 void Jugador::mostrarinfo(){
     cout << "Nombre del Jugador: " << nombrejug << endl;
     cout << "Dorsal del Jugador: " << dorsaljug << endl;
 }
+// Fin de la implementacion de los metodos de la clase Jugador.'
